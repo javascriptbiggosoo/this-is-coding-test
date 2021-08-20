158280bb1ffde8c6e663a1e0e0f6d84bda17631a
ThinkPad of GeneYoung <tbs01215@gmail.com>
2021-08-20 11:41:12 +0900

    21/08/20

--- note.py
+++ note.py
@@ -1,42 +1,6 @@
-a = [] * 10     # '빈 리스트' 만들기
-print(a)
-a = [0] * 10    # '0따리' 리스트
-print(a)
-b = [1]
-print(a+b)
-##
-##
-##
-array = [i for i in range(20) if i % 2]
-print(array)
-array = []
-for i in range(20):
-    if i%2:
-        array.append(i)
-print(array)
-###
-###
-###
-n = 3
-m = 4
-array = [[0]*n for _ in range(m)]
-print(array)
-###
-###
-###
-a = [ 1,3,4]
-a.insert(3,5)
-print("인덱스 3에 5 추가: ", a)
-###
-data = {}
-data['사과'] = 'apple'
-data['바나나'] = 'banana'
-data['당근'] = 'carrot'
-key_list= data.keys()
-print(key_list)
-value_list = data.values()
-print(value_list)
+N = int(input())
+x, y = 1 
+plans = input().split()
 
-for key in key_list:
-    print(data[key])
-###
+dx = [-1,1,0,0]
+dy = [0,0,-1,1] 
