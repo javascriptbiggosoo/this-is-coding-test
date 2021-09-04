1dceac5107d83a6915c44eb5cde208b03ccf6378
tbs01215 <tbs01215@gmail.com>
2021-09-04 20:51:10 +0900

    21/09/04

--- SWEA/보충 수업/D03/1204 . 최빈수 구하기.py
+++ SWEA/보충 수업/D03/1204 . 최빈수 구하기.py
@@ -1,16 +1,17 @@
 import sys
+
 sys.stdin = open("1204.txt", "r")
 
 T = int(input())
-for tc in range(1,T+1):
+for tc in range(1, T + 1):
     int(input())
-    arr = list(map(int,input().split()))
+    arr = list(map(int, input().split()))
     # print(arr)
     # 각 학생의 점수는 0점 이상 100점 이하 => 일정 범위의 정수 => 카운팅 배열을 이용해서 집계
     count = [0] * 101
     for score in arr:
         # print(score)
-        count[score] +=1    #해당 점수 => 인덱스 => 인덱스의 값을 하나 증가
+        count[score] += 1  # 해당 점수 => 인덱스 => 인덱스의 값을 하나 증가
     # print(count)
     # 그 중 가장 큰 값을 찾기 => 그때의 인덱스가 점수
     max = 0
--- 이취코/3-2_실전_문제_큰_수의_법칙.py
+++ 이취코/3-2_실전_문제_큰_수의_법칙.py
@@ -3,22 +3,35 @@
 N, M, K = map(int, input().split())
 arr = list(map(int, input().split()))
 
-max_num = max(arr)
+no1 = max(arr)
 for i in range(N):
-    if arr[i] == max_num:
+    if arr[i] == no1:
         arr.pop(i)
         break
 
-second_num = max(arr)
+no2 = max(arr)
 count = 0
 ans = 0
 
 for _ in range(M):
     if count == K:
-        ans += second_num
+        ans += no2
         count = 0
     else:
-        ans += max_num
+        ans += no1
         count += 1
 
 print(ans)
+
+# sort()를 이용해서 간단하게 풀어봄
+# N, M, K = map(int, input().split())
+# arr = list(map(int, input().split()))
+
+# arr.sort()
+# no1 = arr.pop()
+# no2 = arr.pop()
+
+# 덩어리 = M // (K + 1)
+# 꼬다리 = M % (K + 1)
+# res = (no1 * K + no2) * 덩어리 + 꼬다리 * no1
+# print(res, 덩어리, 꼬다리)
