b069d0ebb145bba2d38e3953da68748099d103a3
tbs01215 <tbs01215@gmail.com>
2021-09-13 19:09:08 +0900

    21/09/13

--- 이코테/11_그리디_문제/01_모험가_길드.py
+++ 이코테/11_그리디_문제/01_모험가_길드.py
@@ -1,3 +1,4 @@
+# 1트, while문으로 그룹 하나가 만들어지는 순간 그만큼 슬라이싱 하고 같은 방식을 반복하려고 시도했는데.. 한참 고민 후 포기
 n = int(input())
 
 모험가들 = sorted(list(map(int, input().split())))
--- 이코테/note.py
+++
@@ -1,9 +0,0 @@
-# 팩토리얼을 만드러보장
-def fact(n):
-    if n == 1:
-        return n
-    return n * fact(n - 1)
-
-
-print(fact(5))
-fact(3)
