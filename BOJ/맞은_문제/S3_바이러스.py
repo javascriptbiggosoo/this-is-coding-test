2983a3806721b6f41a7529c69d2eb282517b3205
KIM JINYOUNG <tbs01215@gmail.com>
2021-09-22 14:50:02 +0900

    21/09/22

--- BOJ/맞은_문제/S3_바이러스.py
+++ BOJ/맞은_문제/S3_바이러스.py
@@ -1,21 +1,26 @@
 ans = 0
 
 
-# def dfs(v):
+def dfs(v):
+    global ans
+    visited[v] = 1
+    for i in graph[v]:
+        if visited[i] != 1:
+            ans += 1
+            dfs(i)
 
 
 컴퓨터수 = int(input())
 간선수 = int(input())
 
 graph = [[] for _ in range(컴퓨터수 + 1)]  # 첫칸은 안씁니당
-print(graph)
 for i in range(간선수):
     st, ed = map(int, input().split())
     graph[st].append(ed)
     graph[ed].append(st)
 
 visited = [0] * (컴퓨터수 + 1)
-print(graph)
 
-# dfs(1)
+
+dfs(1)
 print(ans)
