8953e67b31e36ec47b4167737fc52cd4da6b2102
KIM JINYOUNG <tbs01215@gmail.com>
2021-09-23 11:56:51 +0900

    21/09/23

--- BOJ/맞은_문제/S5_수_정렬하기_2.py
+++
@@ -1,5 +0,0 @@
-N = int(input())
-li = [int(input()) for _ in range(N)]
-li.sort()
-for i in range(N):
-    print(li[i])
--- BOJ/맞은_문제/S5_크로아티아_알파벳.py
+++
@@ -1,9 +0,0 @@
-cro = input()
-res = len(cro)
-
-li = ["c=", "c-", "d-", "lj", "nj", "s=", "z="]
-for i in range(len(li)):
-    res -= cro.count(li[i])
-
-res -= cro.count("dz=")
-print(res)
