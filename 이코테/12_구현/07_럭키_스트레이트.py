b4ecdaec161087df292a96a5e4c1fe32f1c3aefe
KIM JINYOUNG <tbs01215@gmail.com>
2021-09-26 11:11:13 +0900

    21/09/26

--- 이코테/12_구현/07_럭키_스트레이트.py
+++ 이코테/12_구현/07_럭키_스트레이트.py
@@ -1,1 +1,16 @@
 # https://www.acmicpc.net/problem/18406
+# 1트, 성공
+점수 = input()
+중앙 = len(점수) // 2
+앞 = 점수[:중앙]
+뒤 = 점수[중앙:]
+앞총점 = []
+뒷총점 = []
+for i in range(중앙):
+    앞총점.append(int(앞[i]))
+    뒷총점.append(int(뒤[i]))
+
+if sum(앞총점) == sum(뒷총점):
+    print("LUCKY")
+else:
+    print("READY")
