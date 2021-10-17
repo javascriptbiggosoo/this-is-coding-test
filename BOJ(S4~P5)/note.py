47b32a23f7df67aed33facdc8b5cacb51db4956f
KIM JINYOUNG <tbs01215@gmail.com>
2021-10-17 20:46:46 +0900

    21/10/17

--- BOJ(S4~P5)/note.py
+++ BOJ(S4~P5)/note.py
@@ -1,23 +1,23 @@
-# 뱀 푸는 중
-# 뱀 푸는 중
-# 뱀 푸는 중
+# # 뱀 푸는 중
+# # 뱀 푸는 중
+# # 뱀 푸는 중
 
-import sys, pprint
+# import sys, pprint
 
-n = int(sys.stdin.readline().rstrip())
+# n = int(sys.stdin.readline().rstrip())
 
-arr = [[0] * (n + 1) for _ in range(n)]
+# arr = [[0] * (n + 1) for _ in range(n)]
 
 
-k = int(sys.stdin.readline().rstrip())
+# k = int(sys.stdin.readline().rstrip())
 
-for i in range(k):
-    row, col = map(int, sys.stdin.readline().rstrip().split())
-    arr[row][col] = 1
+# for i in range(k):
+#     row, col = map(int, sys.stdin.readline().rstrip().split())
+#     arr[row][col] = 1
 
-pprint.pprint(arr)
-l = int(sys.stdin.readline().rstrip())
+# pprint.pprint(arr)
+# l = int(sys.stdin.readline().rstrip())
 
-for i in range(l):
-    time, ctrl = sys.stdin.readline().rstrip().split()
-    time = int(time)
+# for i in range(l):
+#     time, ctrl = sys.stdin.readline().rstrip().split()
+#     time = int(time)
