dba4a3b4a9901c4e5d81d95d099b8a61c9899892
KIM JINYOUNG <tbs01215@gmail.com>
2021-10-17 22:13:41 +0900

    21/10/17

--- BOJ(S4~P5)/note.py
+++ BOJ(S4~P5)/note.py
@@ -21,3 +21,25 @@
 # for i in range(l):
 #     time, ctrl = sys.stdin.readline().rstrip().split()
 #     time = int(time)
+from collections import deque
+import pprint
+
+
+def dfs(arr, v, visited):
+    visited[v] = 1
+    print(v)
+    for i in range(len(arr[v])):
+        if visited[arr[v][i]] == 0:
+            dfs(arr, arr[v][i], visited)
+
+
+정점수, 간선수, 시작점 = map(int, input().split())
+
+정점들 = [[] for _ in range(정점수 + 1)]
+visited = [0] * (정점수 + 1)
+for i in range(간선수):
+    st, ed = map(int, input().split())
+    정점들[st].append(ed)
+    정점들[ed].append(st)
+
+dfs(정점들, 시작점, visited)
