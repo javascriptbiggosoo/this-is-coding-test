c6ff491d3073a2411561e9eef5ad66b7bd9fcd0d
KIM JINYOUNG <tbs01215@gmail.com>
2021-10-31 15:27:17 +0900

    21/10/31

--- BOJ(S4~P5)/note.py
+++ BOJ(S4~P5)/note.py
@@ -1,19 +1,11 @@
-#  1트 성공, 닫는 괄호는 가장 늦게 여는 괄호와 짝이다. 닫는 괄호가 올때마다 pop을 해주면 된다. 여는 괄호가 없을때 닫는괄호부터 온다면 틀린거다
-T = int(input())
+import sys
 
-for tc in range(T):
-    vps = input()
-    arr = []
-    for i in vps:
-        if i == "(":
-            arr.append("(")
-        else:
-            if len(arr):
-                arr.pop()
-            else:
-                arr.append("false")
-                break
-    if len(arr):
-        print("NO")
+while True:
+    a, b, c = map(int, sys.stdin.readline().rstrip().split())
+    arr = [a, b, c]
+    if a == 0 and b == 0 and c == 0:
+        break
+    if sorted(arr)[2] ** 2 == sorted(arr)[1] ** 2 + sorted(arr)[0] ** 2:
+        print("right")
     else:
-        print("YES")
+        print("wrong")
